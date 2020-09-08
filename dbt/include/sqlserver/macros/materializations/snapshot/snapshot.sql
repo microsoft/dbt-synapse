9b59a1ffdaefe9fbf3f6b37be75ab21571434a60
Anders Swanson <swanson.anders@gmail.com>
2020-09-08 12:54:04 -0700

    temp staging table is now a view instead of temp

--- dbt/include/sqlserver/macros/materializations/snapshot/snapshot.sql
+++ dbt/include/sqlserver/macros/materializations/snapshot/snapshot.sql
@@ -19,7 +19,18 @@
             from {{ source }} 
             where dbt_change_type = ''insert'' ; ');
 
+{% endmacro %}
 
+{% macro sqlserver__build_snapshot_staging_table(strategy, sql, target_relation) %}
+    {% set tmp_relation = default__make_temp_relation(target_relation) %}
+
+    {% set select = snapshot_staging_table(strategy, sql, target_relation) %}
+
+    {% call statement('build_snapshot_staging_relation') %}
+        {{ create_table_as(True, tmp_relation, select) }}
+    {% endcall %}
+
+    {% do return(tmp_relation) %}
 {% endmacro %}
 
 {% materialization snapshot, adapter='sqlserver' %}
@@ -65,15 +76,13 @@
 
       {{ adapter.valid_snapshot_target(target_relation) }}
 
-      {% set staging_table = build_snapshot_staging_table(strategy, sql, target_relation) %}
-
-      {% set nandan_table = create_table_as(False, 'ajs.nandan_fake_temp', 'select * from {{staging_table}}') %}
+      {% set staging_table = sqlserver__build_snapshot_staging_table(strategy, sql, target_relation) %}
 
       -- this may no-op if the database does not require column expansion
       {% do adapter.expand_target_column_types(from_relation=staging_table,
                                                to_relation=target_relation) %}
 
-      {% set missing_columns = adapter.get_missing_columns(nandan_table, target_relation)
+      {% set missing_columns = adapter.get_missing_columns(staging_table, target_relation)
                                    | rejectattr('name', 'equalto', 'dbt_change_type')
                                    | rejectattr('name', 'equalto', 'DBT_CHANGE_TYPE')
                                    | rejectattr('name', 'equalto', 'dbt_unique_key')
@@ -82,7 +91,7 @@
 
       {% do create_columns(target_relation, missing_columns) %}
 
-      {% set source_columns = adapter.get_columns_in_relation(nandan_table)
+      {% set source_columns = adapter.get_columns_in_relation(staging_table)
                                    | rejectattr('name', 'equalto', 'dbt_change_type')
                                    | rejectattr('name', 'equalto', 'DBT_CHANGE_TYPE')
                                    | rejectattr('name', 'equalto', 'dbt_unique_key')
@@ -97,12 +106,12 @@
 
       {% do snapshot_merge_sql(
             target = target_relation,
-            source = nandan_table,
+            source = staging_table,
             insert_cols = quoted_source_columns
          )
       %}
 
-      {% do drop_relation(nandan_table) %}
+      {% do drop_relation(staging_table) %}
 
   {% endif %}
 
