956835ace7c5f6ae44b49a283d5979b07049ffc1
Anders Swanson <swanson.anders@gmail.com>
2020-09-08 14:08:18 -0700

    break out to match dbt-core paradigm

--- dbt/include/sqlserver/macros/materializations/snapshot/snapshot.sql
+++ dbt/include/sqlserver/macros/materializations/snapshot/snapshot.sql
@@ -4,24 +4,6 @@
 {% endmacro %}
 
 
-{% macro sqlserver__snapshot_merge_sql(target, source, insert_cols) -%}
-      {%- set insert_cols_csv = insert_cols | join(', ') -%}
-      {{ log("insert_cols_csv " ~ insert_cols_csv)}}
-      
-      EXEC('  update {{ target }}
-          set dbt_valid_to = TMP.dbt_valid_to
-          from {{ source }} TMP
-          where {{ target }}.dbt_scd_id = TMP.dbt_scd_id
-            and TMP.dbt_change_type = ''update''
-            and {{ target }}.dbt_valid_to is null;
-
-            insert into {{ target }}
-            select {{ insert_cols_csv }}
-            from {{ source }} 
-            where dbt_change_type = ''insert'' ; ');
-
-{% endmacro %}
-
 {% macro sqlserver__build_snapshot_staging_table(strategy, sql, target_relation) %}
     {% set tmp_relation = default__make_temp_relation(target_relation, "pseudotmp") %}
 
---
+++ dbt/include/sqlserver/macros/materializations/snapshot/snapshot_merge.sql
@@ -0,0 +1,17 @@
+{% macro sqlserver__snapshot_merge_sql(target, source, insert_cols) -%}
+      {%- set insert_cols_csv = insert_cols | join(', ') -%}
+      {{ log("insert_cols_csv " ~ insert_cols_csv)}}
+      
+      EXEC('  update {{ target }}
+          set dbt_valid_to = TMP.dbt_valid_to
+          from {{ source }} TMP
+          where {{ target }}.dbt_scd_id = TMP.dbt_scd_id
+            and TMP.dbt_change_type = ''update''
+            and {{ target }}.dbt_valid_to is null;
+
+            insert into {{ target }}
+            select {{ insert_cols_csv }}
+            from {{ source }} 
+            where dbt_change_type = ''insert'' ; ');
+
+{% endmacro %}
