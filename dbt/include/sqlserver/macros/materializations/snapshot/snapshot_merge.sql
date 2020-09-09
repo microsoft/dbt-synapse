310f1cc59f74ee1d80fc19099b0f08f1b4ef7fb2
Anders Swanson <swanson.anders@gmail.com>
2020-09-08 20:51:22 -0700

    align dest col order with source col order

--- dbt/include/sqlserver/macros/materializations/snapshot/snapshot_merge.sql
+++ dbt/include/sqlserver/macros/materializations/snapshot/snapshot_merge.sql
@@ -1,15 +1,16 @@
 {% macro sqlserver__snapshot_merge_sql(target, source, insert_cols) -%}
       {%- set insert_cols_csv = insert_cols | join(', ') -%}
-      {{ log("insert_cols_csv " ~ insert_cols_csv)}}
       
-      EXEC('  update {{ target }}
+      EXEC('update {{ target }}
           set dbt_valid_to = TMP.dbt_valid_to
           from {{ source }} TMP
           where {{ target }}.dbt_scd_id = TMP.dbt_scd_id
             and TMP.dbt_change_type = ''update''
             and {{ target }}.dbt_valid_to is null;
 
-            insert into {{ target }}
+            insert into {{ target }} (
+                  {{ insert_cols_csv }}
+                  )
             select {{ insert_cols_csv }}
             from {{ source }} 
             where dbt_change_type = ''insert'' ; ');
