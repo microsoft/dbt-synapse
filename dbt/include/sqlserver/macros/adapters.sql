5391893306f240d0a1fa0f516f6cc680c0b717e9
Anders Swanson <swanson.anders@gmail.com>
2020-09-24 22:25:43 -0700

    TSQL does not support DROP SCHEMA ... cascade

--- dbt/include/sqlserver/macros/adapters.sql
+++ dbt/include/sqlserver/macros/adapters.sql
@@ -37,6 +37,18 @@
 {% endmacro %}
 
 {% macro sqlserver__drop_schema(relation) -%}
+  {%- set tables_in_schema_query %}
+      SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES
+      WHERE TABLE_SCHEMA = '{{ relation.schema }}'
+  {% endset %}
+  {% set tables_to_drop = run_query(tables_in_schema_query).columns[0].values() %}
+  {% for table in tables_to_drop %}
+    {%- set schema_relation = adapter.get_relation(database=relation.database,
+                                               schema=relation.schema,
+                                               identifier=table) -%}
+    {% do drop_relation(schema_relation) %}
+  {%- endfor %}
+
   {% call statement('drop_schema') -%}
       IF EXISTS (SELECT * FROM sys.schemas WHERE name = '{{ relation.without_identifier().schema }}')
       BEGIN
