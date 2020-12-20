74da6a155653b6f2c71d0e578fff81f0d5f2a65b
Anders Swanson <swanson.anders@gmail.com>
2020-12-19 18:17:10 -0800

    double up single quote iif col is type.Text

--- dbt/include/sqlserver/macros/materializations/seed/seed.sql
+++ dbt/include/sqlserver/macros/materializations/seed/seed.sql
@@ -1,7 +1,7 @@
 {% macro sqlserver__basic_load_csv_rows(model, batch_size, agate_table) %}
     {% set cols_sql = get_seed_column_quoted_csv(model, agate_table.column_names) %}
     {% set bindings = [] %}
-
+    
     {% set statements = [] %}
 
     {% for chunk in agate_table.rows | batch(batch_size) %}
@@ -16,7 +16,16 @@
             {% for row in chunk -%}
                 {{'SELECT'+' '}}
                 {%- for column in agate_table.column_names -%}
-                    '{{row[column].replace("'","''") if row[column]}}'
+                    {# TSQL catch 22:
+                        strings must be single-quoted &
+                        single-quotes inside of strings must be doubled
+                     #}
+                    {% set col_type = agate_table.columns[column].data_type | string %}
+                    {%- if "text.Text" in col_type -%}
+                      '{{str_replace(row[column]) if row[column]}}'
+                    {% else %}
+                      '{{ row[column] if row[column] }}'
+                    {%- endif -%}
                     {%- if not loop.last%}, {%- endif -%}
                 {%- endfor -%}
                 {%- if not loop.last-%} {{' '+'UNION ALL'+'\n'}} {%- endif -%}
