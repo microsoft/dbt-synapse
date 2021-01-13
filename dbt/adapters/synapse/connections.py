9e744470f072e126e8b72d8faa14f408758ac899
Anders Swanson <swanson.anders@gmail.com>
2021-01-12 16:32:40 -0800

    redundant imports

--- dbt/adapters/synapse/connections.py
+++ dbt/adapters/synapse/connections.py
@@ -1,26 +1,8 @@
-from contextlib import contextmanager
-
-import pyodbc
-import os
-import time
-import struct
-from itertools import chain, repeat
-from typing import Callable, Mapping
-
-import dbt.exceptions
-import dbt.adapters.sqlserver.connections as sqlserverconn
-from dbt.adapters.sqlserver import SQLServerConnectionManager
-from dbt.adapters.sqlserver import SQLServerCredentials
-from azure.core.credentials import AccessToken
-from azure.identity import AzureCliCredential, DefaultAzureCredential
-
-from dbt.logger import GLOBAL_LOGGER as logger
 
 from dataclasses import dataclass
-from typing import Optional
 
-
-AZURE_CREDENTIAL_SCOPE = "https://database.windows.net//.default"
+from dbt.adapters.sqlserver import (SQLServerConnectionManager,
+                                    SQLServerCredentials)
 
 
 @dataclass
--- dbt/adapters/synapse/impl.py
+++ dbt/adapters/synapse/impl.py
@@ -1,10 +1,6 @@
 from dbt.adapters.sqlserver import SQLServerAdapter
 from dbt.adapters.synapse import SynapseConnectionManager
-from dbt.adapters.base.relation import BaseRelation
-from typing import (
-    Optional, Tuple, Callable, Iterable, Type, Dict, Any, List, Mapping,
-    Iterator, Union, Set
-)
+
 
 
 class SynapseAdapter(SQLServerAdapter):
