3186fd1f1d877d9c9179950d94878becaf03d4f3
Anders Swanson <swanson.anders@gmail.com>
2021-01-14 16:46:47 -0800

    must re-implement abstract methods

--- dbt/adapters/synapse/connections.py
+++ dbt/adapters/synapse/connections.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 from dbt.adapters.sqlserver import (SQLServerConnectionManager,
                                     SQLServerCredentials)
-
+from dbt.contracts.connection import AdapterResponse
 
 @dataclass
 class SynapseCredentials(SQLServerCredentials):
@@ -15,4 +15,23 @@ class SynapseCredentials(SQLServerCredentials):
 class SynapseConnectionManager(SQLServerConnectionManager):
     TYPE = "synapse"
     TOKEN = None
+
+    @classmethod
+    def get_response(cls, cursor) -> AdapterResponse:
+        #message = str(cursor.statusmessage)
+        message = 'OK'
+        rows = cursor.rowcount
+        #status_message_parts = message.split() if message is not None else []
+        #status_messsage_strings = [
+        #    part
+        #    for part in status_message_parts
+        #    if not part.isdigit()
+        #]
+        #code = ' '.join(status_messsage_strings)
+        return AdapterResponse(
+            _message=message,
+            #code=code,
+            rows_affected=rows
+        )
+
 
