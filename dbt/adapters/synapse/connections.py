5b5a014a799c6add4d81b8f30e50ddfc276dbac8
Anders Swanson <swanson.anders@gmail.com>
2021-01-15 00:36:22 -0800

    not needed

--- dbt/adapters/synapse/connections.py
+++ dbt/adapters/synapse/connections.py
@@ -16,22 +16,4 @@ class SynapseConnectionManager(SQLServerConnectionManager):
     TYPE = "synapse"
     TOKEN = None
 
-    @classmethod
-    def get_response(cls, cursor) -> AdapterResponse:
-        #message = str(cursor.statusmessage)
-        message = 'OK'
-        rows = cursor.rowcount
-        #status_message_parts = message.split() if message is not None else []
-        #status_messsage_strings = [
-        #    part
-        #    for part in status_message_parts
-        #    if not part.isdigit()
-        #]
-        #code = ' '.join(status_messsage_strings)
-        return AdapterResponse(
-            _message=message,
-            #code=code,
-            rows_affected=rows
-        )
-
 
