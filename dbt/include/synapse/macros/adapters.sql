5bbafa24a28f46bf6a8022ddebcba5d1c6188911
Anders Swanson <swanson.anders@gmail.com>
2021-02-04 19:46:02 -0800

    emp view not needed w/ Synapse's CTAS

--- dbt/include/synapse/macros/adapters.sql
+++ dbt/include/synapse/macros/adapters.sql
@@ -89,29 +89,16 @@
 {% endmacro %}
 
 {% macro synapse__create_table_as(temporary, relation, sql) -%}
+   {# temporary isn't used as the syntax is the same #}
    {%- set index = config.get('index', default="CLUSTERED COLUMNSTORE INDEX") -%}
    {%- set dist = config.get('dist', default="ROUND_ROBIN") -%}
-   {% set tmp_relation = relation.incorporate(
-   path={"identifier": relation.identifier.replace("#", "") ~ '_temp_view'},
-   type='view')-%}
-   {%- set temp_view_sql = sql.replace("'", "''") -%}
-
-   {{ synapse__drop_relation_script(tmp_relation) }}
-
-   {{ synapse__drop_relation_script(relation) }}
-
-   EXEC('create view {{ tmp_relation.schema }}.{{ tmp_relation.identifier }} as
-    {{ temp_view_sql }}
-    ');
 
   CREATE TABLE {{ relation.include(database=False) }}
     WITH(
       DISTRIBUTION = {{dist}},
       {{index}}
       )
-    AS (SELECT * FROM {{ tmp_relation.schema }}.{{ tmp_relation.identifier }})
-
-   {{ synapse__drop_relation_script(tmp_relation) }}
+    AS ({{ sql }})
 
 {% endmacro %}
 
