c1d3cea3a76b19b6f54edf17aa148d39db365767
Anders Swanson <swanson.anders@gmail.com>
2021-02-17 01:41:27 -0800

    deprecated

--- dbt/adapters/synapse/connections.py
+++ dbt/adapters/synapse/connections.py
@@ -1,16 +1,8 @@
 
-import time
-
-from contextlib import contextmanager
-import pyodbc
-
 from dataclasses import dataclass
 
-import dbt.exceptions
 from dbt.adapters.sqlserver import (SQLServerConnectionManager,
                                     SQLServerCredentials)
-from dbt.contracts.connection import AdapterResponse
-from dbt.logger import GLOBAL_LOGGER as logger
 
 @dataclass
 class SynapseCredentials(SQLServerCredentials):
