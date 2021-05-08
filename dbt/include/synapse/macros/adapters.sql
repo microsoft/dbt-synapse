0f71b4b2cfebb9e983ee21e65d7a85a294193b97
Anders Swanson <swanson.anders@gmail.com>
2021-05-08 15:29:53 -0400

    override new usage of USE in dbt-sqlserver

--- dbt/include/synapse/macros/adapters.sql
+++ dbt/include/synapse/macros/adapters.sql
@@ -11,12 +11,30 @@
     {{ return(load_result('get_columns_in_query').table.columns | map(attribute='name') | list) }}
 {% endmacro %}
 
-{% macro synapse__list_relations_without_caching(schema_relation) %}
-  {{ return(sqlserver__list_relations_without_caching(schema_relation)) }}
+{% macro sqlserver__list_relations_without_caching(schema_relation) %}
+  {% call statement('list_relations_without_caching', fetch_result=True) -%}
+    select
+      table_catalog as [database],
+      table_name as [name],
+      table_schema as [schema],
+      case when table_type = 'BASE TABLE' then 'table'
+           when table_type = 'VIEW' then 'view'
+           else table_type
+      end as table_type
+
+    from information_schema.tables
+    where table_schema like '{{ schema_relation.schema }}'
+      and table_catalog like '{{ schema_relation.database }}'
+  {% endcall %}
+  {{ return(load_result('list_relations_without_caching').table) }}
 {% endmacro %}
  
-{% macro synapse__list_schemas(database) %}
-  {{ return(sqlserver__list_schemas(database)) }}
+{% macro sqlserver__list_schemas(database) %}
+  {% call statement('list_schemas', fetch_result=True, auto_begin=False) -%}
+    select  name as [schema]
+    from sys.schemas
+  {% endcall %}
+  {{ return(load_result('list_schemas').table) }}
 {% endmacro %}
 
 {% macro synapse__create_schema(relation) -%}
@@ -38,7 +56,16 @@
 {% endmacro %}
 
 {% macro synapse__drop_relation_script(relation) -%}
-  {{ return(sqlserver__drop_relation_script(relation)) }}
+  {% if relation.type == 'view' -%}
+    {% set object_id_type = 'V' %}
+  {% elif relation.type == 'table'%}
+    {% set object_id_type = 'U' %}
+  {%- else -%} invalid target name
+  {% endif %}
+  if object_id ('{{ relation.include(database=False) }}','{{ object_id_type }}') is not null
+    begin
+    drop {{ relation.type }} {{ relation.include(database=False) }}
+    end
 {% endmacro %}
 
 {% macro synapse__check_schema_exists(information_schema, schema) -%}
