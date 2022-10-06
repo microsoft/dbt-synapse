4d62d64a114e0d7bad19ff9ec02ccfe78e6ec1e1
Sam Debruyn <sam@debruyn.dev>
2022-10-06 21:08:31 +0200

    escape schema names

--- dbt/include/synapse/macros/adapters/schema.sql
+++ dbt/include/synapse/macros/adapters/schema.sql
@@ -2,7 +2,7 @@
   {% call statement('create_schema') -%}
     IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = '{{ relation.without_identifier().schema }}')
     BEGIN
-    EXEC('CREATE SCHEMA {{ relation.without_identifier().schema }}')
+    EXEC('CREATE SCHEMA [{{ relation.without_identifier().schema }}]')
     END
   {% endcall %}
 {% endmacro %}
