b1615dfb72d6db8cbdff9ebb870b65ac51ee4e7b
Sam Debruyn <sam@debruyn.dev>
2022-10-06 20:32:02 +0200

    fix datediff test

--- dbt/include/synapse/macros/materializations/seeds/helpers.sql
+++ dbt/include/synapse/macros/materializations/seeds/helpers.sql
@@ -1,38 +1,37 @@
-{% macro synapse__basic_load_csv_rows(model, batch_size, agate_table) %}
+{% macro synapse__load_csv_rows(model, agate_table) %}
 
-    {# Synapse does not support the TSQL's normal Table Value Constructor of #}
-    {# INSERT INTO Dest_Table (cols) SELECT cols FROM Ref_Table #}
+    {# Synapse does not support the normal Table Value Constructor #}
+
+    {% set batch_size = get_batch_size() %}
 
     {% set cols_sql = get_seed_column_quoted_csv(model, agate_table.column_names) %}
-    {% set bindings = [] %}
 
     {% set statements = [] %}
 
     {% for chunk in agate_table.rows | batch(batch_size) %}
-        {% set bindings = [] %}
-
-        {% for row in chunk %}
-            {% do bindings.extend(row) %}
-        {% endfor %}
 
         {% set sql %}
             insert into {{ this.render() }} ({{ cols_sql }})
             {% for row in chunk -%}
-                {{'SELECT'+' '}}
+                {{'select'+' '}}
                 {%- for column in agate_table.column_names -%}
-                    {# TSQL catch 22: #}
-                        {# strings must be single-quoted & #}
-                        {# single-quotes inside of strings must be doubled #}
+                    {#
+                        TSQL catch 22:
+                        strings must be single-quoted &
+                        single-quotes inside of strings must be doubled
+                    #}
 
                     {% set col_type = agate_table.columns[column].data_type | string %}
-                    {%- if "text.Text" in col_type -%}
+                    {%- if not row[column] -%}
+                        NULL
+                    {%- elif "text.Text" in col_type -%}
                       '{{str_replace(row[column]) if row[column]}}'
                     {% else %}
                       '{{ row[column] if row[column] }}'
                     {%- endif -%}
-                    {%- if not loop.last%}, {%- endif -%}
+                    {%- if not loop.last%},{%- endif -%}
                 {%- endfor -%}
-                {%- if not loop.last-%} {{' '+'UNION ALL'+'\n'}} {%- endif -%}
+                {%- if not loop.last -%} {{' '+'union all'+'\n'}} {%- endif -%}
             {%- endfor -%}
         {% endset %}
 
@@ -45,8 +44,4 @@
 
     {# Return SQL so we can render it out into the compiled files #}
     {{ return(statements[0]) }}
-{% endmacro %}
-
-{% macro synapse__load_csv_rows(model, agate_table) %}
-  {{ return(synapse__basic_load_csv_rows(model, 200, agate_table) )}}
 {% endmacro %}
