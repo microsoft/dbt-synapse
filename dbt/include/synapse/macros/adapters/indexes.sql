cd3b6afca49f8d7727aee95728c17068aa13c2a5
artc95 <arthur@dataroots.io>
2024-02-05 10:13:55 +0100

    fix indexes.sql for "USE statement not supported" error - switch dependency

--- dbt/include/synapse/macros/adapters/indexes.sql
+++ dbt/include/synapse/macros/adapters/indexes.sql
@@ -126,3 +126,35 @@ declare @drop_remaining_indexes_last nvarchar(max) = (
 {% macro create_nonclustered_index(columns, includes=False) %}
   {{ return(create_nonclustered_index(columns, includes=False)) }}
 {% endmacro %}
+
+
+{% macro drop_fk_indexes_on_table(relation) -%}
+  {% call statement('find_references', fetch_result=true) %}
+      SELECT  obj.name AS FK_NAME,
+      sch.name AS [schema_name],
+      tab1.name AS [table],
+      col1.name AS [column],
+      tab2.name AS [referenced_table],
+      col2.name AS [referenced_column]
+      FROM sys.foreign_key_columns fkc
+      INNER JOIN sys.objects obj
+          ON obj.object_id = fkc.constraint_object_id
+      INNER JOIN sys.tables tab1
+          ON tab1.object_id = fkc.parent_object_id
+      INNER JOIN sys.schemas sch
+          ON tab1.schema_id = sch.schema_id
+      INNER JOIN sys.columns col1
+          ON col1.column_id = parent_column_id AND col1.object_id = tab1.object_id
+      INNER JOIN sys.tables tab2
+          ON tab2.object_id = fkc.referenced_object_id
+      INNER JOIN sys.columns col2
+          ON col2.column_id = referenced_column_id AND col2.object_id = tab2.object_id
+      WHERE sch.name = '{{ relation.schema }}' and tab2.name = '{{ relation.identifier }}'
+  {% endcall %}
+      {% set references = load_result('find_references')['data'] %}
+      {% for reference in references -%}
+        {% call statement('main') -%}
+           alter table [{{reference[1]}}].[{{reference[2]}}] drop constraint [{{reference[0]}}]
+        {%- endcall %}
+      {% endfor %}
+{% endmacro %}
