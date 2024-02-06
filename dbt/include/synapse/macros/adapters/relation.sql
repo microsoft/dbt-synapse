40e1737c879b31e60e112071fc4fa4ddebe67bd0
artc95 <arthur@dataroots.io>
2024-02-06 14:13:42 +0100

    add truncate_relation macro in relation.sql, to overwrite fabric's macro that does not specify Distribution option in "CREATE TABLE AS SELECT" - switch dependency

--- dbt/include/synapse/macros/adapters/relation.sql
+++ dbt/include/synapse/macros/adapters/relation.sql
@@ -34,3 +34,9 @@
   rename object {{ from_relation.include(database=False) }} to {{ to_relation.identifier }}
   {%- endcall %}
 {% endmacro %}
+
+{% macro synapse__truncate_relation(relation) %}
+    {% call statement('truncate_relation') -%}
+        truncate table {{ relation }}
+    {%- endcall %}
+{% endmacro %}
