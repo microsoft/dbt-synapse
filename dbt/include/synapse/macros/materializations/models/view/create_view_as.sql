b115a23870db8778637080a68da999a1aa3797cf
nszoni <namson98@gmail.com>
2024-02-09 17:35:08 +0100

    add constraint ddl macro and extend materialization

--- dbt/include/synapse/macros/materializations/models/view/create_view_as.sql
+++ dbt/include/synapse/macros/materializations/models/view/create_view_as.sql
@@ -1,4 +1,15 @@
 {% macro synapse__create_view_as(relation, sql) -%}
-  create view {{ relation.include(database=False) }} as
-    {{ sql }}
+
+  {%- set temp_view_sql = sql.replace("'", "''") -%}
+
+  {% set contract_config = config.get('contract') %}
+
+    {{exceptions.warn("Model contracts cannot be enforced by <adapter>!")}}
+
+  {% if contract_config.enforced %}
+      {{ get_assert_columns_equivalent(sql) }}
+  {%- endif %}
+
+  EXEC('create view {{ relation.include(database=False) }} as {{ temp_view_sql }};');
+
 {% endmacro %}
