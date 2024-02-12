18da1725d0b841a3b2611349e477a816327e70ec
nszoni <namson98@gmail.com>
2024-02-12 15:26:29 +0100

    add run hooks tests

---
+++ tests/functional/adapter/test_run_hooks.py
@@ -0,0 +1,9 @@
+from dbt.tests.adapter.hooks.test_run_hooks import TestAfterRunHooks, TestPrePostRunHooks
+
+
+class TestPrePostRunHooks(TestPrePostRunHooks):
+    pass
+
+
+class TestAfterRunHooks(TestAfterRunHooks):
+    pass
