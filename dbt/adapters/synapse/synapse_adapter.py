ad7ef7fa52f4d38c78481999f23170a0e7b7d2dc
artc95 <arthur@dataroots.io>
2024-02-13 16:39:20 +0100

    update to use _make_ref_key_dict (instead of _make_ref_key_msg from dbt-core v1.4.9)

--- dbt/adapters/synapse/synapse_adapter.py
+++ dbt/adapters/synapse/synapse_adapter.py
@@ -1,5 +1,5 @@
 from dbt.adapters.base.relation import BaseRelation
-from dbt.adapters.cache import _make_ref_key_msg
+from dbt.adapters.cache import _make_ref_key_dict
 from dbt.adapters.fabric import FabricAdapter
 from dbt.adapters.sql.impl import CREATE_SCHEMA_MACRO_NAME
 from dbt.events.functions import fire_event
@@ -13,7 +13,7 @@ class SynapseAdapter(FabricAdapter):
 
     def create_schema(self, relation: BaseRelation) -> None:
         relation = relation.without_identifier()
-        fire_event(SchemaCreation(relation=_make_ref_key_msg(relation)))
+        fire_event(SchemaCreation(relation=_make_ref_key_dict(relation)))
         macro_name = CREATE_SCHEMA_MACRO_NAME
         kwargs = {
             "relation": relation,
