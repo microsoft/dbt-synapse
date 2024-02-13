194c7d4cd51c4afa034b12d41409115552d8b634
nszoni <namson98@gmail.com>
2024-02-13 17:05:37 +0100

    overwrite is_integer list

--- dbt/adapters/synapse/synapse_column.py
+++ dbt/adapters/synapse/synapse_column.py
@@ -2,4 +2,15 @@ from dbt.adapters.fabric import FabricColumn
 
 
 class SynapseColumn(FabricColumn):
-    ...
+    # extending list of integer types for synapse
+    def is_integer(self) -> bool:
+        return self.dtype.lower() in [
+            # real types
+            "smallint",
+            "bigint",
+            "tinyint",
+            "serial",
+            "bigserial",
+            "int",
+            "bit",
+        ]
