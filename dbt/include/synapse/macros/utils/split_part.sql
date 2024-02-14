440e4a97a8bca982ce1492211fb426037813dda9
artc95 <arthur@dataroots.io>
2024-02-14 14:04:22 +0100

    fix macro synapse__split_part to handle negative ordinal (failing test_utils.py::TestSplitPartSynapse)

--- dbt/include/synapse/macros/utils/split_part.sql
+++ dbt/include/synapse/macros/utils/split_part.sql
@@ -2,8 +2,18 @@
     We have to replace the macro from dbt-sqlserver since that one uses XML which is an unsupported data type in Synapse.
     The function below is not supported in Synapse Dedicated SQL according to the documentation, but it seems to work.
 #}
+
 {% macro synapse__split_part(string_text, delimiter_text, part_number) %}
 
-    (select value from string_split({{ string_text }}, {{ delimiter_text }}, 1) where ordinal = {{ part_number }})
+    {% if part_number >= 0 %}
+
+        (select value from string_split({{ string_text }}, {{ delimiter_text }}, 1) where ordinal = {{ part_number }})
+
+    {% else %}
+
+        (select value from string_split({{ string_text }}, {{ delimiter_text }}, 1)
+        where ordinal = len(replace({{ string_text }}, {{delimiter_text}}, '')) + 1 + {{ part_number }})
+
+    {% endif %}
 
 {% endmacro %}
