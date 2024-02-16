79bee0c1c96f256bb40ffc7ce512125fc4ce7700
nszoni <namson98@gmail.com>
2024-02-16 11:56:33 +0100

    add null compare test zone

---
+++ tests/functional/adapter/test_null_compare.py
@@ -0,0 +1,9 @@
+from dbt.tests.adapter.utils.test_null_compare import BaseMixedNullCompare, BaseNullCompare
+
+
+class TestMixedNullCompareSynapse(BaseMixedNullCompare):
+    pass
+
+
+class TestNullCompareSynapse(BaseNullCompare):
+    pass
