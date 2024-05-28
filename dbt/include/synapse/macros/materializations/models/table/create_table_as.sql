e05bcbfae54453dafa4dbe3c442e1e0cb57ef708
Pradeep Srikakolapu <prdpsvs@gmail.com>
2024-05-27 21:06:56 -0700

    Update tests

--- dbt/include/synapse/macros/materializations/models/table/create_table_as.sql
+++ dbt/include/synapse/macros/materializations/models/table/create_table_as.sql
@@ -1,30 +1,25 @@
+-- Need to check why model contract are not enforced.
+-- TODO: Is it because Synapse uses Fabric table materialization and usage of this macro build model constraints?
 {% macro synapse__create_table_as(temporary, relation, sql) -%}
-   {%- set index = config.get('index', default="CLUSTERED COLUMNSTORE INDEX") -%}
-   {%- set dist = config.get('dist', default="ROUND_ROBIN") -%}
-   {% set tmp_relation = relation.incorporate(
-   path={"identifier": relation.identifier.replace("#", "") ~ '_temp_view'},
-   type='view')-%}
-   {%- set temp_view_sql = sql.replace("'", "''") -%}
+    {%- set index = config.get('index', default="CLUSTERED COLUMNSTORE INDEX") -%}
+    {%- set dist = config.get('dist', default="ROUND_ROBIN") -%}
+    {% set tmp_relation = relation.incorporate(path={"identifier": relation.identifier ~ '__dbt_tmp_vw'}, type='view')-%}
+    {%- set temp_view_sql = sql.replace("'", "''") -%}
 
-    {% do adapter.drop_relation(tmp_relation) %}
-    {% do adapter.drop_relation(relation) %}
+    {{ get_create_view_as_sql(tmp_relation, sql) }}
+    {% set contract_config = config.get('contract') %}
 
-   {{ synapse__create_view_as(tmp_relation, sql) }}
-
-   {% set contract_config = config.get('contract') %}
-
-   {% if contract_config.enforced %}
+    {% if contract_config.enforced %}
 
         {{exceptions.warn("Model contracts cannot be enforced by <adapter>!")}}
 
         CREATE TABLE [{{relation.schema}}].[{{relation.identifier}}]
         {{ synapse__build_columns_constraints(tmp_relation) }}
         WITH(
-          DISTRIBUTION = {{dist}},
-          {{index}}
+            DISTRIBUTION = {{dist}},
+            {{index}}
         )
         {{ get_assert_columns_equivalent(sql)  }}
-
         {% set listColumns %}
             {% for column in model['columns'] %}
                 {{ "["~column~"]" }}{{ ", " if not loop.last }}
@@ -33,11 +28,8 @@
 
         INSERT INTO [{{relation.schema}}].[{{relation.identifier}}]
         ({{listColumns}}) SELECT {{listColumns}} FROM [{{tmp_relation.schema}}].[{{tmp_relation.identifier}}]
-
     {%- else %}
         EXEC('CREATE TABLE [{{relation.database}}].[{{relation.schema}}].[{{relation.identifier}}]WITH(DISTRIBUTION = {{dist}},{{index}}) AS (SELECT * FROM [{{tmp_relation.database}}].[{{tmp_relation.schema}}].[{{tmp_relation.identifier}}]);');
     {% endif %}
-
-   {% do adapter.drop_relation(tmp_relation) %}
-
+    {% do adapter.drop_relation(tmp_relation)%}
 {% endmacro %}
